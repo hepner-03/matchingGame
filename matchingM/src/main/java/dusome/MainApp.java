adefd0d86989585b19e233cf78cd5dcca9866790
max <mhepner03@gmail.com>
2020-12-11 09:32:15 -0500

    Finished Game

--- matchingM/src/main/java/dusome/MainApp.java
+++ matchingM/src/main/java/dusome/MainApp.java
@@ -15,7 +15,7 @@ public class MainApp extends Application {
     @Override
     public void start(@SuppressWarnings("exports") Stage s) throws IOException {
         stage=s;
-        setRoot("primary","");
+        setRoot("game","");
     }
 
     static void setRoot(String fxml) throws IOException {
